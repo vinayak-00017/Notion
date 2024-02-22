61ed2681ed3bdadcc52004f0aa7fdc0973780e0f
vinayak <fobo401@gmail.com>
2024-02-22 12:07:24 +0530

    Notion Clone

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import type {Config} from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+dotenv.config({path:'.env.local'})
+
+if(!process.env.DATABASE_URL){
+    console.log("Cannot find database URL")
+    
+}
+
+export default{
+    schema: './src/lib/superbase/schema.ts',
+    out: './migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL  || '',
+    }
+} satisfies Config;
